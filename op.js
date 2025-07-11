6e2063cfb33246bcd5f3fde7c432c850dbdfe358
gogulpradeep <mgogulpradeep@gmail.com>
2025-07-11 17:57:29 +0530

    updates

---
+++ op.js
@@ -0,0 +1,96 @@
+ /*let a =10;
+ let b= 20;
+ let c = 2;
+
+ let sum = (a-b)/2;
+ console.log(sum);
+
+  let increment= 10;
+  increment++;
+  increment*=3;
+
+  console.log(increment);  
+
+  let num1=6;
+  let num2=6;
+
+  console.log(num1!==num2); 
+
+
+  let a =10;
+  let b=12;
+  if(a>5&& b>10){
+    console.log(" the greatest of all time");}
+    else if(a>90||b>11){
+        console.log("the lowest of all time");
+    } else{
+        console.log(" good night");
+    }
+
+        let a =true;
+        let b = false;
+
+        if(a=true&& b = false){
+            console.log("true");}
+            elseif(a= flase|| b=false){
+                console.log("not flase");
+            }
+            else{
+                
+            } 
+
+
+function greet (){
+    console.log(" i am going to adord one day");
+}
+
+greet(); 
+
+function greetstudent(name){
+    console.log("Hello"+" " + name + " "+ "! Nice to see you.");
+}
+greetstudent("gogul pradeep");
+greetstudent("gamingpuli");*/
+
+
+
+  let a= 10;
+  let b = 20;
+if(a<b){
+console.log("hello gogul pradeep");
+}else{
+    console.log("bye gogulpradeep");
+}
+
+
+
+let marks = 75;
+
+if(marks>90){
+    console.log("Grade A");
+}else if
+    ( marks>=75){
+console.log("Grade b");
+}
+else{
+    console.log("Grade c");
+
+}
+
+
+
+let name = "puli";
+switch(name){
+    case "puli":
+    console.log("my name is gaming puli");
+    break;
+    case "giri":
+        console.log("my name is giri");
+        break;
+        case "hari":
+            console.log("my name is hari");
+            break ;
+}
+
+
+
